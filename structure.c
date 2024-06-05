587932820d74b89454015a48cb15022baaf40d3d
otakugodie <95259091+otakugodie@users.noreply.github.com>
2024-06-05 20:16:54 +0100

    some basic exercises 2

--- structure.c
+++ structure.c
@@ -3,6 +3,8 @@
 int main(){
     int edad = 43;
     int edad2= 15;
-    printf("- La segunda edad es: %d \n - La primera edad es: %d", edad2, edad);
+    printf("- La segunda edad es: %d \n- La primera edad es: %d\n", edad2, edad);
+    printf("\"Bienvenidos\" al \tcurso\n");
+    printf("Hola, estas ahi\?");
     return 0;
 }
